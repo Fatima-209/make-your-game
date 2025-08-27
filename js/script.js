8de6dfb09c75fd6d05ca0cda722f42d31d1361ef
falaiwi <fatimaalaiwi99@hotmail.com>
2025-08-27 18:05:32 +0300

    Select and cache needed elements with JS

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// connecting html elements with js
+var scoreElement = document.getElementById('score');
+var timerElement = document.getElementById('timer');
+var livesElement = document.getElementById('lives');
+var gbElement = document.getElementById('game-board');
+
+document.addEventListener('keydown', function(event){
+    
+}) 
+console.log(timerElement);
