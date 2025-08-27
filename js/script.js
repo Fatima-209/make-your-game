34b02ba01baf0bf61acf9742ed33eb3e3e81d47f
falaiwi <fatimaalaiwi99@hotmail.com>
2025-08-27 18:38:58 +0300

    Event Listeners

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ var scoreElement = document.getElementById('score');
 var timerElement = document.getElementById('timer');
 var livesElement = document.getElementById('lives');
 var gbElement = document.getElementById('game-board');
-
+let directionX = 0;
+let directionY = 0;
+// event listners to update the snake position based on the users keys
 document.addEventListener('keydown', function(event){
-    
+  console.log('Key pressed:', event.key);
 }) 
-console.log(timerElement);
